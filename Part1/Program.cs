1cf9dca3b03457bf899442992a815cf3b6f02d9b
ersin <ersinclair@gmail.com>
2020-09-07 15:48:28 -0700

    Add project files.

---
+++ Part1/Program.cs
@@ -0,0 +1,63 @@
+﻿/// Lab No. 2		Exercise No. 1
+/// File Name: MyFirstProgram
+/// @author: Evan Sinclair
+/// Date:  September 4, 2020
+///
+/// Problem Statement: Ask the user to enter three numbers. Calculate the sum and product of
+/// these three numbers. Divide the sum of the three numbers by the product of the same three numbers.
+/// Display Result
+/// 
+/// Overall Plan:
+/// 1) Print an initial welcoming message to the screen
+/// 2) Prompt the user for three integers
+/// 3) Calculate the sum and product of the integers
+/// 4) Print the sum of the integers to the screen
+/// 5) Print the product of the integers to the screen
+/// 6) Print the sum / product to the screen
+///
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part1
+{
+    class MyFirstProgram
+    {
+        static void Main(string[] args)
+        {
+            // print a message to the screen
+            Console.WriteLine("Hello out there.");
+            Console.WriteLine("I will add three numbers for you and then divide that result by the product.");
+            Console.WriteLine("Enter a number and then press enter 3 times.");
+
+            // declare five integer variables
+            int n1, n2, n3, product, sum;
+
+            // read in n1, n2, and n3
+            
+            n1 = Int32.Parse(Console.ReadLine());
+            n2 = Int32.Parse(Console.ReadLine());
+            n3 = Int32.Parse(Console.ReadLine());
+
+            // calculate the sum and product of these three numbers
+            sum = n1 + n2 + n3;
+            product = n1 * n2 * n3;
+
+            // print the sum, the product, and the sum / product 
+            Console.WriteLine("The sum of the three numbers is");
+            Console.WriteLine(sum);
+            Console.WriteLine("The product of the three numbers is");
+            Console.WriteLine(product);
+            Console.WriteLine("The sum divided by the product is");
+            Console.WriteLine(sum / product);
+
+            //Just to pause the screen.
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,42 @@
+﻿/// Lab No. 2		Exercise No. 2
+/// File Name: ArcadeRewardCalculator
+/// @author: Evan Sinclair
+/// Date:  September 4, 2020
+///
+/// Problem Statement: Get the total number of points recieved from an arcade game. You can 
+/// redeem 10 coupons for a candy bar or 3 coupons for a gumball. You prefer candy bars to gumballs.
+/// Calculate the number of bars and gumballs that can be purchased             
+/// 
+/// Overall Plan:
+/// 1) Get number of points from the user
+/// 2) Calculate the maximum number of bars purchasable
+/// 3) Calculate the number of gumballs purchasable with the leftover points
+/// 4) Print the results to the console
+///
+
+using System;
+
+namespace Part2
+{
+    class ArcadeRewardCalculator
+    {
+        static void Main(string[] args)
+        {
+            
+            int points, bars, gumballs;
+            
+            // Get points from user
+            Console.WriteLine("Enter your total points");
+            points = Int32.Parse(Console.ReadLine());
+            
+            // Calculate bars and gumballs
+            bars = points / 10;
+            gumballs = (points % 10) / 3;
+
+            // Output bars and gumballs 
+            Console.WriteLine("You have enough points for " + bars + " bars and " + gumballs + " gumballs");
+
+            Console.ReadLine();
+        }
+    }
+}
